124dfa32749f2972efca49bcfbed0c01bb811089
Агафонов Владислав Алексеевич <78472316+Chpion@users.noreply.github.com>
2021-04-16 17:56:13 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,65 +1,148 @@
-function Matrix() {
+function CreateMatrix() {
 	var n = document.getElementById("n").value;
-	var tab = "<table>";
-	if (n > 0) {
-	for (var i = 0; i < n; i++) {
-		tab += '<tr>'
-		for (var j = 0; j < n; j++) {
-			tab += '<td><input type="number" name = "value" placeholder = "0 or 1" min="0" max="1"></td>';
-        }
-		tab += '</tr>';       
+	if (n < 0) {
+		alert("n должно быть положительным!");
 	}
-	tab += "</table>";
-	}       // цикл для матрицы n x n
-	else {
-		alert("Введите значение > 0");   // если введено n < 0, выведет сообщение об этом
-	} 
+    var tab = '<table>';
+    for (var i = 0; i < n; i++) {
+		tab += '<tr>';
+		for(var j = 0; j < n; j++) {
+			tab += '<td> <input type="number" class="value" min="0" max="1"></td>';
+		}
+		tab += '</tr>';       
+    }
+    tab += '</table>';
 	document.getElementById('matrix').innerHTML = tab; 
+
 }
 function valid() {
-	var n = document.getElementById("n").value;
-	var arr1 = document.getElementsByName("value");
-	var s = 0;
-	var arr = []; 
-	for (var i = 0; i < n; i++){
+    var n = document.getElementById("n").value;
+    var arr1 = document.getElementsByClassName("value"); // получяем коллекцию элементов
+    var s = 0;
+    var arr = [];  
+    for (var i = 0; i < n; i++){
         arr[i] = [];
         for (var j = 0; j < n; j++){
-			arr[i][j] = arr1[s].value;  //запись элементов коллекции в массив
-			s++; 
+        	arr[i][j] = arr1[s].value;  //запись элементов коллекции в массив
+		s++; 
 		}	
+	}
+    for (var i = 0; i < n; i++){   
+        for (var j = 0; j < n; j++){
+            if(arr[i][j] > 1 || arr[i][j] < 0 ){  // условие для элементов меньше 0 и больше 1
+                return alert("Ошибка!Значения могут быть только 0 и 1!");
+            }
+	    }	 
     }
-		for (var i = 0; i < n; i++){   
-			for (var j = 0; j < n; j++){
-                if(arr[i][j] > 1 || arr[i][j] < 0 ){  // условие для элементов меньше 0 и больше 1
-                    return alert("ДОПУСТИМЫЕ ЗНАЧЕНИЯ ТОЛЬКО 0 и 1. ИСПРАВЬТЕ!!!");
-                }
-		}	 
-    }
-    return true;
+    return true;  
 }
 
-function graph() {
+function reflex() {
+	var n = document.getElementById("n").value;
+	var arr1 = document.getElementsByClassName("value");
+	var count = 0;
+	var flag = true;
+	for (var i = 0; i < n; i++){
+		for (var j = 0; j < n; j++){
+			if(i == j) {
+				if(arr1[count].value != 1) {
+					flag = false;
+				}				
+			}
+			count++;
+		}	
+	}	
+	if(flag) {
+		document.getElementById('reflex').innerHTML = 'Рефлексивно';
+	}
+	else {
+		document.getElementById('reflex').innerHTML = 'Не является рефлексивным';
+	}
+}
+
+function symm() {
     var n = document.getElementById("n").value;
-	var arr1 = document.getElementsByName("value");
+	var arr1 = document.getElementsByClassName("value");  // получаем коллекцию всех элементов
 	var s = 0;
+	var result = 'Отношение является СИММЕТРИЧНЫМ';
 	var arr = [];
 	for (var i = 0; i < n; i++){
 	    arr[i] = [];
 	    for (var j = 0; j < n; j++){
-			arr[i][j] = arr1[s].value;
-			s++;
+		arr[i][j] = arr1[s].value;  // записываем элементы в двумерный массив
+		s++;
 	    }	
 	}
 	for (var i = 0; i < n; i++){
 	    for (var j = 0; j < n; j++){
-	        if(arr[i][j] != arr[j][i]) { //условие для неориентированного графа(не может быть асимметричным)
-				return alert("ГРАФ НЕ ЯВЛЯЕТСЯ НЕОРИЕНТИРОВАННЫМ");
+	        if(arr[i][j] != arr[j][i]) {  // если существует ребро из i в j, и не существует ребра ведущего из j в i, то граф несимметричен
+				result = 'Отношение не является СИММЕТРИЧНЫМИ';
 		    }
 	    }	
 	}
-    return true;        
-}   
+	document.getElementById('symm').innerHTML = result;	
+}  
 
-function resh() {
+function cososymm() {
+	var n = document.getElementById("n").value;
+	var arr = document.getElementsByClassName("value");
+	var count = 0;
+	var flag = true;
+	var arr2 = [];
+	for (var i = 0; i < n; i++){
+		arr2[i] = [];
+		for (var j = 0; j < n; j++){
+			arr2[i][j] = arr[count].value;
+			count++;
+		}	
+	}	
+	for (var i = 0; i < n; i++){				
+		for (var j = 0; j < n; j++){
+				if(arr2[i][j] != -(arr2[j][i])) {
+					flag = false;
+				}			
+		}	
+	}	
+	if(flag) {
+		document.getElementById('cososymm').innerHTML = 'Кососимметрично';
+	}
+	else {
+		document.getElementById('cososymm').innerHTML = 'Не является кососимметричным';
+	}
+}
+
+function trans(){
+    var n = document.getElementById("n").value;
+    var arr1 = document.getElementsByClassName("value"); // получаем коллекцию всех элементов
+    var s = 0;
+    var result = 'Отношение является ТРАНЗИТИВНЫМ';
+	var arr = [];
+	for (var i = 0; i < n; i++){
+	    arr[i] = [];
+	    for (var j = 0; j < n; j++){
+			arr[i][j] = arr1[s].value;   // записываем элементы в двумерный массив
+		    s++;
+	    }	
+	}
+    for (var i = 0; i < n; i++){
+		for (var j = 0; j < n; j++){
+            if(arr[i][j] == 1) {  // если существует ребро, соединяющее вершины i и j
+                for (var z = 0; z < n; z++) {
+                    if (arr[j][z] == !arr[i][z]) {   // если из вершины j в вершину z ведёт ребро, но из i в z - нет, то граф нетранзитивен
+						result = "Отношение НЕ является ТРАНЗИТИВНЫМ";
+                    }
+				}
+            }
+        }
+    }
+    document.getElementById('trans').innerHTML = result;
+}
+
+
+function result() {
 	valid();
+    reflex();
+    symm();
+	cososymm();
+    trans();
 }
